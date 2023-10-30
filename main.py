fa42e8a1d85a8d3b0c435388d675bc9cad54eed5
joshburns <92578127+jbazel@users.noreply.github.com>
2023-10-30 16:35:23 +0000

    chord and lyric removal

--- main.py
+++ main.py
@@ -2,15 +2,24 @@ import music21.converter
 from music21 import *
 import sqlite3
 us = environment.UserSettings()
-us['musicxmlPath'] ='../../../../../Applications/MuseScore 4.app'
+us['musicxmlPath'] ='../../../../../Applications/MuseScore 3.app'
 us['midiPath'] ='../../../../../Applications/GarageBand.app'
-c = music21.converter.parse("OpenEWLD-master/dataset/Fats_Waller/The_Jitterbug_Waltz/The_Jitterbug_Waltz.mxl")
-c.show('MIDI'
-)
+c = music21.converter.parse("OpenEWLD-master/dataset/Alicia_Scott/Annie_Laurie/Annie_Laurie.mxl")
 
-#
+# lyric removal
+for n in c.recurse().notesAndRests:
+    if n.lyric:
+        print(n.lyric)
+        n.lyric = ''
+
+# chord removal
+for n in c.recurse().getElementsByClass(chord.Chord):
+    meas = n.getContextByClass(stream.Measure)
+    meas.remove(n)
+
+c.show()
+
+
 # db = "/OpenEWLD-master/OpenEWLD.db"
 # db = sqlite3.connect(db)
 
-
-print([x.id for x in features.extractorsById('all')])
