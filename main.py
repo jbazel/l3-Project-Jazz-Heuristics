be1b0eda15318f63afd2b8c99757ae8f23921a9a
joshburns <92578127+jbazel@users.noreply.github.com>
2023-10-30 15:06:03 +0000

    update

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import music21.converter
 from music21 import *
-c = music21.converter.parse("OpenEWLD-master/dataset/Alicia_Scott/Annie_Laurie/Annie_Laurie.mxl")
-c.show("Annie_Laurie.pdf")
+import sqlite3
+us = environment.UserSettings()
+us['musicxmlPath'] ='../../../../../Applications/MuseScore 4.app'
+us['midiPath'] ='../../../../../Applications/GarageBand.app'
+c = music21.converter.parse("OpenEWLD-master/dataset/Fats_Waller/The_Jitterbug_Waltz/The_Jitterbug_Waltz.mxl")
+c.show('MIDI'
+)
+
+#
+# db = "/OpenEWLD-master/OpenEWLD.db"
+# db = sqlite3.connect(db)
+
+
+print([x.id for x in features.extractorsById('all')])
