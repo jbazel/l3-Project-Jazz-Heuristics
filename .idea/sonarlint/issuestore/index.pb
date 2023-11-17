125d66bdadb625a73a32299e6280bb57e9262814
joshburns <92578127+jbazel@users.noreply.github.com>
2023-11-17 11:02:33 +0000

    updated chord-melody separation algorithm

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+–
+fEWLD/dataset/A.P._Carter/I'm_Thinking_Tonight_of_My_Blue_Eyes/I'm_Thinking_Tonight_of_My_Blue_Eyes.mxl,5/2/5285ea4aa6d28ee43fc34d499340ac22310b2e8e
+–
+fEWLD/dataset/A.P._Carter/I'm_Thinking_Tonight_of_My_Blue_Eyes/I'm_Thinking_Tonight_of_My_Blue_Eyes.csv,a/2/a2721abf3db8c3c1a9c4588a632aa152ac714310
+D
+EWLD/db_creation.sql,8/5/85a8e73aa57583b7bed58689f218ad01f58098f3
+>
+EWLD/README.md,8/3/83f8b46022c5ad5f4fc9487d579f8e644819c8e0
+D
+EWLD/EWLDcreation.py,8/a/8a6b6507e6f3990996160bb8a47207907904b006
+7
+main.py,f/1/f1bdda93d9a278e358509d498e17d97764c1fb29
--- main.py
+++ main.py
@@ -1,22 +1,86 @@
 import music21.converter
 from music21 import *
 import sqlite3
-us = environment.UserSettings()
-us['musicxmlPath'] ='../../../../../Applications/MuseScore 3.app'
-us['midiPath'] ='../../../../../Applications/GarageBand.app'
-c = music21.converter.parse("EWLD/dataset/Alicia_Scott/Annie_Laurie/Annie_Laurie.mxl")
 
+us = environment.UserSettings()
+us['musicxmlPath'] = '../../../../../Applications/MuseScore 3.app'
+us['midiPath'] = '../../../../../Applications/GarageBand.app'
 db = sqlite3.connect('EWLD/EWLD.db')
 cursor = db.cursor()
-cursor.execute('SELECT * FROM "work_genres" where "genre" = "Jazz"')
-print(len(cursor.fetchall()))
+cursor.execute('SELECT DISTINCT path_leadsheet FROM works, work_genres WHERE work_genres.genre = "Jazz"')
+paths = cursor.fetchall()
 
-# # lyric removal
-# for n in c.recurse().notesAndRests:
-#     if n.lyric:
-#         print(n.lyric)
-#         n.lyric = ''
+class Leadsheet:
+
+    def __init__(self, source):
+        self.source = source
+        self.score_parsed = music21.converter.parse(source)
+        self.chords = []
+        self.chord_profiles = []
+        self.chord_intervals = []
+        self.chord_common_names = []
+        self.melodies = []
+
+    def lyric_removal(self):
+        for n in self.score_parsed.recurse().notesAndRests:
+            if n.lyric:
+                n.lyric = ""
+
+    def chord_feature_extraction(self):
+        profiles = []
+        intervals = []
+        for i in self.chords:
+            profile = [0]*12
+            interval = [0]*12
+            normal_order = i.normalOrder
+            for j in normal_order:
+                profile[j] = 1
+
+            first_pitch = normal_order[0]
+            rotated_normal = [(pc - first_pitch) % 12 for pc in normal_order]
+            for j in rotated_normal:
+                interval[j] = 1
+
+            profiles.append(profile)
+            intervals.append(interval)
+
+        self.chord_intervals = intervals[1:]
+        self.chord_profiles = profiles[1:]
+        self.chord_common_names = [chord.commonName for chord in self.chords]
+
+    def chord_melody_separation(self):
+        score = [n for n in self.score_parsed.recurse().notes]
+        separation = [x for x in range(len(score)) if type(score[x]) == music21.harmony.ChordSymbol]
+        self.chords = [score[i] for i in separation]
+        separation.append(len(score))
+        for i in range(len(separation) - 1):
+            self.melodies.append(score[separation[i] + 1:separation[i + 1]])
+
+
+
+leadsheets = []
+chord_dict = {}
+
+for i in range(100):
+    path = paths[i]
+    # print('parsing ', paths)
+    path = "EWLD/" + path[0]
+    sheet = Leadsheet(path)
+    sheet.chord_melody_separation()
+    sheet.chord_feature_extraction()
+    leadsheets.append(sheet)
+    for j in sheet.chord_common_names:
+        if j in chord_dict:
+            chord_dict[j] += 1
+        else:
+            chord_dict[j] = 1
+    print(sheet.chord_profiles)
+
 #
+# print(leadsheets)
+# print(chord_dict)
+
+
 # # chord removal
 # for n in c.recurse().getElementsByClass(chord.Chord):
 #     meas = n.getContextByClass(stream.Measure)
@@ -24,11 +88,8 @@ print(len(cursor.fetchall()))
 #     meas.remove(n)
 
 
-
-
 # c.show()
 
 
 # db = "/OpenEWLD-master/OpenEWLD.db"
 # db = sqlite3.connect(db)
-
